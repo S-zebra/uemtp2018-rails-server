1f0dfd1ab05ef36e0e201c84bfd2b1a2102bbebd
S-zebra <shimashimazebra13@gmail.com>
2018-09-10 11:16:14 +0900

    ログイン画面を追加

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    a_params = params.permit([:user_name, :password])
+    account = Account.authenticate(a_params[:user_name], a_params[:password])
+    if account
+      session[:uid] = account.id
+      flash[:notice] = "ログインしました。"
+      redirect_to :tokens
+    else
+      flash[:error] = "ログインに失敗しました。再度お試しください。"
+      redirect_to action: "new"
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "accounts#new"
+
   resources :accounts
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+
+  resources :tokens
+
   namespace :api do
     resources :images, only: [:show, :create, :update, :destroy]
     get "/posts/list", to: "posts#list"
-    post "/signup", to: "accounts#create"
   end
+
+  root "accounts#new"
 end
