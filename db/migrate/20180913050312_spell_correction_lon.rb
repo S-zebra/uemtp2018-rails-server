500680940895b6575860cf6815a53c42f94fa28f
S-zebra <shimashimazebra13@gmail.com>
2018-09-13 14:04:49 +0900

    DBスペル修正スクリプトを入れ直し

---
+++ db/migrate/20180913050312_spell_correction_lon.rb
@@ -0,0 +1,7 @@
+class SpellCorrectionLon < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :posts, :longtitude
+    add_column :posts, :longitude, null: false, default: -1
+    #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
+  end
+end
