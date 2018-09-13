3bbc9e24409ae26a391de4529261c0bb3e9bcebd
S-zebra <shimashimazebra13@gmail.com>
2018-09-13 13:53:27 +0900

    migrate修正

--- db/migrate/20180902134850_spell_correction.rb
+++ db/migrate/20180902134850_spell_correction.rb
@@ -1,6 +1,7 @@
 class SpellCorrection < ActiveRecord::Migration[5.2]
   def change
-    change_column :posts, :longtitude, :longitude
+    remove_column :posts, :longtitude
+    change_column :posts, :longitude, :float, null: false, default: -1
     #Ex:- change_column("admin_users", "email", :string, :limit =>25)
   end
 end
