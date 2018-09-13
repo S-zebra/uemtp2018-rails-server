78c8bc7fd0cd307c70f75996e80a92ddb8d64c43
S-zebra <shimashimazebra13@gmail.com>
2018-09-13 15:01:01 +0900

    post.rb修正2

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,5 @@ class Post < ApplicationRecord
   validates :text, :latitude, :longtitude, presence: true
   validates :text, length: {maximum: 300}
   validates :latitude, numericality: {greater_than_or_equal_to: -90, less_than_or_equal_to: 90}
-  validates :longitude, numericality: {greater_than_or_equal_to: -180, less_than_or_equal_to: 180}
+  validates :longtitude, numericality: {greater_than_or_equal_to: -180, less_than_or_equal_to: 180}
 end
