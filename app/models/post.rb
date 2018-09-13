b3ab75600ea8f70d39bd9c498fb02509b39acedf
S-zebra <shimashimazebra13@gmail.com>
2018-09-13 14:57:04 +0900

    post.rb修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :account
-  validates :text, :latitude, :longitude, presence: true
+  validates :text, :latitude, :longtitude, presence: true
   validates :text, length: {maximum: 300}
   validates :latitude, numericality: {greater_than_or_equal_to: -90, less_than_or_equal_to: 90}
   validates :longitude, numericality: {greater_than_or_equal_to: -180, less_than_or_equal_to: 180}
