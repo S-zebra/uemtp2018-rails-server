45d72d6952423dcbc724ddc5d7f2139eed66b109
S-zebra <shimashimazebra13@gmail.com>
2018-09-13 13:59:32 +0900

    DBカラム名修正

--- db/migrate/20180901051456_create_posts.rb
+++ db/migrate/20180901051456_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration[5.2]
     create_table :posts do |t|
       t.references :account, null: false
       t.float :latitude, null: false, default: -1
-      t.float :longtitude, null: false, default: -1
+      t.float :longitude, null: false, default: -1
       t.text :text, null: false
       t.timestamps
     end
--- db/migrate/20180902134850_spell_correction.rb
+++
@@ -1,7 +0,0 @@
-class SpellCorrection < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :posts, :longtitude
-    change_column :posts, :longitude, :float, null: false, default: -1
-    #Ex:- change_column("admin_users", "email", :string, :limit =>25)
-  end
-end
