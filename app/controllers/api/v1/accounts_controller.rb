7aae0995206fd5ac8df8a5ca40848f7d81fe9a62
S-zebra <shimashimazebra13@gmail.com>
2018-09-15 23:29:01 +0900

    availableの返り値変更

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -2,9 +2,9 @@ class Api::V1::AccountsController < ApiController
   def available?
     token = Token.find_by(token: params[:token])
     if token && token.account
-      render json: {status: 200, result: "available"}
+      render json: {status: 200, result: true}
     else
-      render json: {status: 200, result: "unavailable"}
+      render json: {status: 200, result: false}
     end
   end
 end
