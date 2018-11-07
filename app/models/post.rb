68030b8ed8e1a10e694a4009fa6444b0ed5ed091
S-zebra <shimashimazebra13@gmail.com>
2018-11-07 12:39:47 +0900

    コラム名のミスを修正 (Model)

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :account
-  validates :text, :latitude, :longtitude, presence: true
+  validates :text, :latitude, :longitude, presence: true
   validates :text, length: {maximum: 300}
   validates :latitude, numericality: {greater_than_or_equal_to: -90, less_than_or_equal_to: 90}
-  validates :longtitude, numericality: {greater_than_or_equal_to: -180, less_than_or_equal_to: 180}
+  validates :longitude, numericality: {greater_than_or_equal_to: -180, less_than_or_equal_to: 180}
 end
