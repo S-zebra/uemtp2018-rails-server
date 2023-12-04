18b885fd1cb29c38aa5516388d0d8afc571624c6
S-zebra <shimashimazebra13@gmail.com>
2023-12-04 17:21:40 +0900

    modify db migrate script

--- db/migrate/20180913050312_spell_correction_lon.rb
+++ db/migrate/20180913050312_spell_correction_lon.rb
@@ -1,6 +1,6 @@
 class SpellCorrectionLon < ActiveRecord::Migration[5.2]
   def change
-    remove_column :posts, :longtitude
+#    remove_column :posts, :longtitude
     add_column :posts, :longitude, :float, null: false, default: -1
     #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
   end
